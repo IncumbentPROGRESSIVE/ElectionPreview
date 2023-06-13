f0cd81002f5651a7d5c7aced18062d2e9a203841
Colin Leary <113806491+IncumbentPROGRESSIVE@users.noreply.github.com>
2023-06-13 17:47:51 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
 // Declare two boolean variables
 let Republican = false; // Change the value to true or false as needed
 let Democrat = false; // Change the value to true or false as needed
-let Moderate = true: //
+let Moderate = true; // Corrected this line
 
+// Get the input and output elements
+var userInput = document.getElementById('userInput');
+var outputBox = document.getElementById('outputBox');
 
-  // Get the input and output elements
-  var userInput = document.getElementById('userInput');
-  var outputBox = document.getElementById('outputBox');
-  var userTest = 5
+// Listen for when the input box's value changes
+userInput.addEventListener('input', function() {
+  // Store the user's input in a variable
+  var userText = userInput.value;
 
-  // Listen for when the input box's value changes
-  userInput.addEventListener('input', function() {
-    // Store the user's input in a variable
-    var userText = userInput.value;
-
-    // Update the output box with the user's text
-    outputBox.value = userText;
-  });
+  // Update the output box with the user's text
+  outputBox.value = userText;
 });
-
